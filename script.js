be25ba7d6529febdc159bc8e5d0bbb56db88c624
wolfdog27 <kylenicholasanderson@gmail.com>
2021-03-10 14:56:38 -0800

    form validation but not sending

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+
+
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Get the forms we want to add validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
 const btn = document.getElementById('button');
 
 document.getElementById('form')
