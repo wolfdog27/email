ca3812f9dc67965cd056280e53a7b315f6e43ea6
wolfdog27 <kylenicholasanderson@gmail.com>
2021-03-12 11:44:55 -0800

    denis to the rescue

--- script.js
+++ script.js
@@ -1,65 +1,56 @@
-// $("#bootstrapForm").submit(function (event) {
 
-//     // make selected form variable
-//     var vForm = $(this);
-
-//     if (vForm[0].checkValidity() === false) {
-//         event.preventDefault()
-//         event.stopPropagation()
-//     } else {
-
-//     }
-//     vForm.addClass('was-validated');
-// });
-
-
-
-
-(function () {
-    'use strict';
-    window.addEventListener('load', function () {
-        // Get the forms we want to add validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
-})();
-
-function validatePassword(from_name) {
-    if (!from_name) return 'Full name is required';
-
-    return '';
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
 
+// function validate() {
+//     const $result = $("#result");
+//     const email = $("#reply_to").val();
+//     $result.text("");
+
+//     if (validateEmail(email)) {
+//         $result.text(email + " is valid :)");
+//         $result.css("color", "green");
+//     } else {
+//         $result.text(email + " is not valid :(");
+//         $result.css("color", "red");
+//     }
+//     return false;
+// }
+
+// $("#validate").on("click", validate);
+
+
+
+// This is for EmailJS
 const btn = document.getElementById('button');
+const email = document.getElementById('reply_to')
+const emailE = document.getElementById('emailError')
 
 document.getElementById('form')
     .addEventListener('submit', function (event) {
         event.preventDefault();
+        emailE.textContent = ""
 
+        const validEmail = validateEmail(email.value)
+        if (!validEmail) {
+            emailE.textContent = "Please enter a valid e-mail"
+            return
+        }
         btn.value = 'Sending...';
 
         const serviceID = 'default_service';
         const templateID = 'contact_form';
-        if (event.target.classList.contains('was-validated')) {
-            emailjs.sendForm(serviceID, templateID, this)
-                .then(() => {
-                    btn.value = 'Send Email';
-                    $('#success_message').show();
-                    window.setTimeout(function () { location.reload() }, 3000)
-                }, (err) => {
-                    btn.value = 'Send Email';
-                    $('#error_message').show();
-                    window.setTimeout(function () { location.reload() }, 3000)
-                });
-        }
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Send Email';
+                $('#success_message').show();
+                window.setTimeout(function () { location.reload() }, 3000)
+            }, (err) => {
+                btn.value = 'Send Email';
+                $('#error_message').show();
+                window.setTimeout(function () { location.reload() }, 3000)
+            });
     });
 
